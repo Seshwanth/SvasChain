30fe389ab42c72d86b7d369b17eb7cf7ec8ea922
spacecentre <akkilalagar05@gmail.com>
2025-03-06 17:50:32 +0530

    setup clean app, add shadcn

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button>Click Me!</Button>
+    </div>
+  )
+}
+
+export default HomePage
